dec9089039416937f115ebd30bf9ba90b09223ee
Andrew Lim <andrew.b.lim@gmail.com>
2016-10-11 16:01:01 -0400

    Apply jsTimezoneDetect to signup form

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var plugins = require('gulp-load-plugins')();
+
+var mainBowerFiles = require('main-bower-files');
+var pump = require('pump');
+
+gulp.task('js', function (cb) {
+  pump(
+    [
+      gulp.src(mainBowerFiles()),
+      plugins.uglify(),
+      gulp.dest('public/scripts')
+    ],
+    cb
+  );
+});

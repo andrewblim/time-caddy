bbe5064a919e083d27b48700b01f5dbc139f6c56
Andrew Lim <andrew.b.lim@gmail.com>
2016-10-24 08:59:46 -0400

    Add destroy-unconfirmed-stale specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,15 +4,15 @@ require 'bcrypt'
 
 RSpec.describe User do
   it 'disallows two users with the same username' do
-    expect(create(:user, username: 'test', email: 'test@test.com')).to be_a(described_class)
+    expect(create(:user, username: 'test', email: 'test@test.com')).to be_a described_class
     expect { create(:user, username: 'test', email: 'test2@test.com') }.to raise_error ActiveRecord::RecordInvalid
-    expect(create(:user, username: 'test2', email: 'test2@test.com')).to be_a(described_class)
+    expect(create(:user, username: 'test2', email: 'test2@test.com')).to be_a described_class
   end
 
   it 'disallows two users with the same email' do
-    expect(create(:user, username: 'test', email: 'test@test.com')).to be_a(described_class)
+    expect(create(:user, username: 'test', email: 'test@test.com')).to be_a described_class
     expect { create(:user, username: 'test2', email: 'test@test.com') }.to raise_error ActiveRecord::RecordInvalid
-    expect(create(:user, username: 'test2', email: 'test2@test.com')).to be_a(described_class)
+    expect(create(:user, username: 'test2', email: 'test2@test.com')).to be_a described_class
   end
 
   it 'disallows users with bad email addresses' do
@@ -32,7 +32,7 @@ RSpec.describe User do
       default_tz: 'America/New_York',
       signup_time: Time.now,
     )
-    expect(user).to be_a(described_class)
+    expect(user).to be_a described_class
     expect(user.save).to be true
     expect(user.check_password('foo')).to be true
     expect(user.check_password('foo2')).to be false
@@ -47,7 +47,7 @@ RSpec.describe User do
       default_tz: 'America/New_York',
       signup_time: Time.now,
     )
-    expect(user).to be_a(described_class)
+    expect(user).to be_a described_class
     expect(user.save).to be true
     expect(user.password_salt).to eq(custom_salt)
     expect(user.check_password('foo')).to be true
@@ -97,7 +97,74 @@ RSpec.describe User do
     expect(user.confirmed?(Time.now.advance(days: User::INACTIVE_ACCOUNT_LIFESPAN_IN_DAYS + 10))).to be true
   end
 
-  it 'correctly destroys unconfirmed stale users when requested' do
+  it 'correctly destroys unconfirmed stale users by username when requested' do
+    user_fresh = create(:user, username: 'fresh', email: 'fresh@test.com', signup_time: Time.now)
+    user_stale1 = create(
+      :user,
+      username: 'stale1',
+      email: 'stale1@test.com',
+      signup_time: Time.now.advance(days: -User::INACTIVE_ACCOUNT_LIFESPAN_IN_DAYS - 1),
+    )
+    user_stale2 = create(
+      :user,
+      username: 'stale2',
+      email: 'stale2@test.com',
+      signup_time: Time.now.advance(days: -User::INACTIVE_ACCOUNT_LIFESPAN_IN_DAYS - 1),
+    )
+
+    # destroy nothing
+    User.destroy_unconfirmed_stale_by_username(
+      'stale1',
+      as_of: Time.now.advance(days: -User::INACTIVE_ACCOUNT_LIFESPAN_IN_DAYS),
+    )
+    expect(described_class.find(user_fresh.id)).to be_a described_class
+    expect(described_class.find(user_stale1.id)).to be_a described_class
+    expect(described_class.find(user_stale2.id)).to be_a described_class
+
+    # destroy something
+    User.destroy_unconfirmed_stale_by_username('stale1') # default Time.now
+    expect(described_class.find(user_fresh.id)).to be_a described_class
+    expect { described_class.find(user_stale1.id) }.to raise_error  ActiveRecord::RecordNotFound
+    expect(described_class.find(user_stale2.id)).to be_a described_class
+    User.destroy_unconfirmed_stale_by_username('stale2') # default Time.now
+    expect(described_class.find(user_fresh.id)).to be_a described_class
+    expect { described_class.find(user_stale1.id) }.to raise_error  ActiveRecord::RecordNotFound
+    expect { described_class.find(user_stale2.id) }.to raise_error  ActiveRecord::RecordNotFound
+  end
+
+  it 'correctly destroys unconfirmed stale users by email when requested' do
+    user_fresh = create(:user, username: 'fresh', email: 'fresh@test.com', signup_time: Time.now)
+    user_stale1 = create(
+      :user,
+      username: 'stale1',
+      email: 'stale1@test.com',
+      signup_time: Time.now.advance(days: -User::INACTIVE_ACCOUNT_LIFESPAN_IN_DAYS - 1),
+    )
+    user_stale2 = create(
+      :user,
+      username: 'stale2',
+      email: 'stale2@test.com',
+      signup_time: Time.now.advance(days: -User::INACTIVE_ACCOUNT_LIFESPAN_IN_DAYS - 1),
+    )
+
+    # destroy nothing
+    User.destroy_unconfirmed_stale_by_email(
+      'stale1@test.com',
+      as_of: Time.now.advance(days: -User::INACTIVE_ACCOUNT_LIFESPAN_IN_DAYS),
+    )
+    expect(described_class.find(user_fresh.id)).to be_a described_class
+    expect(described_class.find(user_stale1.id)).to be_a described_class
+    expect(described_class.find(user_stale2.id)).to be_a described_class
+
+    # destroy something
+    User.destroy_unconfirmed_stale_by_email('stale1@test.com') # default Time.now
+    expect(described_class.find(user_fresh.id)).to be_a described_class
+    expect { described_class.find(user_stale1.id) }.to raise_error  ActiveRecord::RecordNotFound
+    expect(described_class.find(user_stale2.id)).to be_a described_class
+    User.destroy_unconfirmed_stale_by_email('stale2@test.com') # default Time.now
+    expect(described_class.find(user_fresh.id)).to be_a described_class
+    expect { described_class.find(user_stale1.id) }.to raise_error  ActiveRecord::RecordNotFound
+    expect { described_class.find(user_stale2.id) }.to raise_error  ActiveRecord::RecordNotFound
   end
 
   it 'counts password resets'
